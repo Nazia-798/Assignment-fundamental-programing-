9d0a8ba59cee01d140a94b81657d05f99df82fb4
Nazia-798 <134121838+Nazia-798@users.noreply.github.com>
2023-06-04 14:31:49 +0500

    Add files via upload

---
+++ Nazia .c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main (void)
+{
+
+char name[100]="Nazia ",FathersName[100]="Muhammad Uris", Age[100]="20", Grade[100]="A";
+
+printf("\tName: %s\n\n",name);
+	printf("\tFather's Name: %s\n\n",FathersName);
+	printf("\tAge: %s\n\n",Age);
+	printf("\tGrade: %s\n\n",Grade);
+
+
+}  
